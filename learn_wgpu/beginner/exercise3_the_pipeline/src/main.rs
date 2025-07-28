4b47f4fc7f9a0104908ef86d1b41da16d1a69f74
devinmcmillen1990 <devin.mcmillen@protonmail.com>
2025-07-27 22:36:28 -0700

    Setup base template for exercise 3

---
+++ learn_wgpu/beginner/exercise3_the_pipeline/src/main.rs
@@ -0,0 +1,21 @@
+mod app;
+mod state;
+
+use winit::event_loop::{EventLoop, ControlFlow};
+
+use crate::app::App;
+
+pub fn main() {
+    let _ = run();
+}
+
+fn run() -> anyhow::Result<()> {
+    env_logger::init();
+
+    let event_loop = EventLoop::with_user_event().build()?;
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app);
+    Ok(())
+}
