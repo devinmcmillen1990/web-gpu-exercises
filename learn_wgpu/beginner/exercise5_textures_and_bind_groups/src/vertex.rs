185075f12f4eff3b2d9f6ecf6a35922e3057ef64
devinmcmillen1990 <devin.mcmillen@protonmail.com>
2025-08-04 09:57:42 -0700

    Add final solution to exercise 5 where we extract the texture as a separate module.

--- learn_wgpu/beginner/exercise5_textures_and_bind_groups/src/vertex.rs
+++ learn_wgpu/beginner/exercise5_textures_and_bind_groups/src/vertex.rs
@@ -5,12 +5,13 @@ pub struct Vertex {
     texture_coords: [f32; 2],
 }
 
+// NOTE: We had to update the vertices to ensure that the image displayed right-side-up
 pub const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.0868241, 0.49240386, 0.0], texture_coords: [0.4131759, 0.99240386], }, // A
-    Vertex { position: [-0.49513406, 0.06958647, 0.0], texture_coords: [0.0048659444, 0.56958647], }, // B
-    Vertex { position: [-0.21918549, -0.44939706, 0.0], texture_coords: [0.28081453, 0.05060294], }, // C
-    Vertex { position: [0.35966998, -0.3473291, 0.0], texture_coords: [0.85967, 0.1526709], }, // D
-    Vertex { position: [0.44147372, 0.2347359, 0.0], texture_coords: [0.9414737, 0.7347359], }, // E
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], texture_coords: [0.4131759, 0.00759614], }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], texture_coords: [0.0048659444, 0.43041354], }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], texture_coords: [0.28081453, 0.949397], }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], texture_coords: [0.85967, 0.84732914], }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], texture_coords: [0.9414737, 0.2652641], }, // E
 ];
 
 pub const INDICES : &[u16] = &[
