d2fb2de046a36f575bbc4826218c9c8b0d411c69
devinmcmillen1990 <devin.mcmillen@protonmail.com>
2025-08-05 11:18:07 -0700

    Checkin a couple template files for exercise 8. Still need a lot more.

--- learn_wgpu/beginner/exercise8_the_depth_buffer/src/camera.rs
+++ learn_wgpu/beginner/exercise8_the_depth_buffer/src/camera.rs
@@ -6,7 +6,7 @@ use winit::keyboard::{ KeyCode, PhysicalKey, };
 pub struct Camera {
     pub eye: cgmath::Point3<f32>,
     pub target: cgmath::Point3<f32>,
-    pub up: cgmath::Vector3<f32>,
+    pub up: cgmath::Vectorc<f32>,
     pub aspect: f32,
     pub fovy: f32,
     pub znear: f32,
@@ -23,19 +23,8 @@ const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::from_cols(
 
 impl Camera {
     fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
-        let view = cgmath::Matrix4::look_at_rh(
-            self.eye,
-            self.target,
-            self.up
-        );
-
-        let proj = cgmath::perspective(
-            cgmath::Deg(self.fovy),
-            self.aspect,
-            self.znear,
-            self.zfar
-        );
-
+        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
         return OPENGL_TO_WGPU_MATRIX * proj * view;
     }
 }
@@ -109,6 +98,7 @@ impl CameraController {
                     _ => {
                         false
                     },
+
                 }
             },
             _ => {
