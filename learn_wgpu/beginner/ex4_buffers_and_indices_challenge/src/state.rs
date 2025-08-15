b16d21b223c2f6ef31d0dba3e9618e20e2d48d8e
devinmcmillen1990 <devin.mcmillen@protonmail.com>
2025-08-15 09:05:00 -0700

    Add logic and comments for the ex4 solution. Will update README in next commit.

--- learn_wgpu/beginner/ex4_buffers_and_indices_challenge/src/state.rs
+++ learn_wgpu/beginner/ex4_buffers_and_indices_challenge/src/state.rs
@@ -18,6 +18,14 @@ pub struct State {
     vertex_buffer: wgpu::Buffer,
     index_buffer: wgpu::Buffer,
     num_indices: u32,
+
+    // Storing separate instances of the challenge buffers so that we can swap them during rendering
+    challenge_vertex_buffer: wgpu::Buffer,
+    challenge_index_buffer: wgpu::Buffer,
+    challenge_num_indices: u32,
+
+    // flag used to determine whether or not to render the complex challenge shape
+    challenge_use_complex: bool,
 }
 
 impl State {
@@ -64,6 +72,76 @@ impl State {
             contents: bytemuck::cast_slice(VERTEX_INDICES),
             usage: wgpu::BufferUsages::INDEX,
         });
+
+        // For our complex shape we'll follow the challenge 
+
+        // represents the points spaced uniformly around a circle (16-gon)
+        let num_vertices = 16;
+
+        // one full turn is 2π radians, so the step between consecutive vertices is Δ𝜃 = 2𝜋/𝑁
+        // then, for the i-th vertex, θ_i = i ⋅ Δθ
+        let angle = std::f32::consts::PI * 2.0 / num_vertices as f32;
+        
+        // Polar -> Cartesian
+        let challenge_vertices = (0..num_vertices).map(|i| {
+            // for the i-th vertex, <theta>θ_i = i ⋅ Δθ
+            let theta = angle * i as f32;
+
+            // A point on a circle with radius "r" that is centered at the origin is: (x = r ⋅ cos(θ), y = r ⋅ sin(θ))
+            // Here r = 0.5. That keeps the polygon safely inside the clip-space unit square (NDC is [−1,1] in both x and y).
+            let circle_radius = 0.5;
+
+            Vertex {
+                position: [
+                    circle_radius * theta.cos(),
+
+                    // the negative sign will flip the y-axis
+                    // this is like reflecting the polygon across the x-axis, which will affect the clockwise vs. counter-clockwise orientation that will matter for backface culling
+                    -(circle_radius) * theta.sin(),
+                    0.0
+                ],
+                // remember that cos and sin produce values in the range [−1,1]. In order to get the range [0,1] we can apply the formula (x + 1)/2.
+                // this will apply a smooth angular color gradient around the shape
+                // interpolation across triangles creates the continuous “mango” look
+                color: [
+                    (1.0 + theta.cos()) / 2.0,
+                    (1.0 + theta.sin()) / 2.0,
+                    0.0
+                ]
+            }
+        }).collect::<Vec<_>>();
+
+        // A convex polygon with 𝑁 vertices can be broken into exactly N−2 triangles when you fan from a single vertex (vertex 0)
+        //      EX: Pentagon (5 Vertices) -> 3 Triangles
+        //      EX: Hexagon  (6 Vertices) -> 4 Triangles
+        // we anchor at vertex 0 and connect consecutive outer vertices
+        // because y uses -sin(θ) (clockwise ordering), we flip the last two vertices to keep CCW winding under back-face culling
+        let num_triangles = num_vertices - 2;
+
+        let challenge_indices = (1u16..num_triangles + 1)
+            // one CCW triangle per i (given y = -sin(θ))
+            .flat_map(|i| {
+                // represents one triangle, stored in index order for correct winding.
+                [0, i + 1, i]
+            })
+            .collect::<Vec<_>>();
+
+        let challenge_num_indices = challenge_indices.len() as u32;
+
+        let challenge_vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Challenge Vertex Buffer"),
+            contents: bytemuck::cast_slice(&challenge_vertices),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        let challenge_index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Challenge Index Buffer"),
+            contents: bytemuck::cast_slice(&challenge_indices),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
+        let challenge_use_complex = false;
+
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline"),
             layout: Some(&render_pipeline_layout),
@@ -115,6 +193,11 @@ impl State {
             vertex_buffer,
             index_buffer,
             num_indices: VERTEX_INDICES.len() as u32,
+
+            challenge_vertex_buffer,
+            challenge_index_buffer,
+            challenge_num_indices,
+            challenge_use_complex,
         })
     }
 
@@ -127,8 +210,11 @@ impl State {
         }
     }
 
-    pub fn handle_key(&self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
+    pub fn handle_key(&mut self, event_loop: &ActiveEventLoop, code: KeyCode, is_pressed: bool) {
         match (code, is_pressed) {
+            (KeyCode::Space, pressed) => {
+                self.challenge_use_complex = pressed;
+            },
             (KeyCode::Escape, true) => {
                 event_loop.exit();
             },
@@ -167,9 +253,25 @@ impl State {
             });
 
             renderpass.set_pipeline(&self.render_pipeline);
-            renderpass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            renderpass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-            renderpass.draw_indexed(0..self.num_indices, 0, 0..1);
+
+            // This will swap the buffers depending on the status of "challenge_use_complex"
+            let data = if self.challenge_use_complex {
+                (
+                    &self.challenge_vertex_buffer,
+                    &self.challenge_index_buffer,
+                    self.challenge_num_indices,
+                )
+            } else {
+                (
+                    &self.vertex_buffer,
+                    &self.index_buffer,
+                    self.num_indices,
+                )
+            };
+
+            renderpass.set_vertex_buffer(0, data.0.slice(..));
+            renderpass.set_index_buffer(data.1.slice(..), wgpu::IndexFormat::Uint16);
+            renderpass.draw_indexed(0..data.2, 0, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
